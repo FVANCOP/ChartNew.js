e88b13e61aaa8c131757cfdcd2b3da6d47eb8a9c
Ole Kröger <o.kroeger@wikunia.de>
2014-08-25 14:34:38 +0200

    % stop values

--- Add-ins/color.js
+++ Add-ins/color.js
@@ -5,13 +5,32 @@ function gradient(area,params) {
 		var gradientColors = params.data.datasets[params.i].gradientColors;
 		var steps = gradientColors.length;
 		var currentStepValue = 0;
+		var stepValues = [];
+		var PERCENT_REGEX = /(\d{1,2}|100)%\s*?$/g
 		for (var i = 0; i < steps; i++) {
-userStepValue=gradientColors[i].match(/\d+% ?/g); 
-if(userStepValue){userStepValue=parseFloat(userStepValue) / 100.0;}
-if(!userStepValue){userStepValue=currentStepValue;}
-GradientcolorsWithoutStep=gradientColors[i].replace(/\d+% ?/g, "");
-			grd.addColorStop(userStepValue,GradientcolorsWithoutStep);
-			currentStepValue += 1/(steps-1);
+			var userStepValue = gradientColors[i].match(PERCENT_REGEX);
+			if(!userStepValue) 	{ stepValues[i] = false; continue; }
+			userStepValue = parseFloat(userStepValue) / 100.0;
+			stepValues[i] = userStepValue;
+		}
+		for (var i = 0; i < steps; i++) {
+			if (stepValues[i] === false) {
+				if (i == 0) { stepValues[i] = 0; }
+				else if (i == steps-1) { stepValues[i] = 1; }
+				else {
+					// found next stepValue which isn't false
+					var s=i+1;
+					while (s < steps-1 && stepValues[s] === false) {
+						s++;
+					}
+					var lastStep  = ((i == 0) ? 0 : stepValues[i-1]);
+					stepValues[i] = ((s >= steps-1) ? 1 : stepValues[s+1]) - lastStep;
+					stepValues[i] = lastStep + stepValues[i]/(s-i+1);
+				}
+			}
+			GradientcolorsWithoutStep = gradientColors[i].replace(PERCENT_REGEX, "").trim();
+			console.log(stepValues[i],GradientcolorsWithoutStep);
+			grd.addColorStop(stepValues[i],GradientcolorsWithoutStep);
 		}
 		return grd;
 	}
